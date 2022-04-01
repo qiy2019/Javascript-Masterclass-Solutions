07456a21f760fae8890ab002e433c6c6cc222dfd
Yunqi <67021191+qiy2019@users.noreply.github.com>
2022-03-31 20:47:29 -0700

    Create DLL_push.js

---
+++ DLL_push.js
@@ -0,0 +1,31 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.length = 0;
+    }
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+let doublyLinkedList = new DoublyLinkedList
+console.log(doublyLinkedList.push(5))
